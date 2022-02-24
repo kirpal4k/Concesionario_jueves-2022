a72eb3658957cd67ed58c5c18f4437e5fc33acb7
206 <206@academicos.cesde.edu.co>
2022-02-24 08:46:43 -0500

    se actualiza

--- app/src/main/java/com/example/concesionario_jueves/ClienteActivity.java
+++ app/src/main/java/com/example/concesionario_jueves/ClienteActivity.java
@@ -3,12 +3,65 @@ package com.example.concesionario_jueves;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ClienteActivity extends AppCompatActivity {
+
+    EditText jetidentificacion,jetnombre,jetusuario,jetclave1,jetclave2;
+    TextView jtvactico;
+    Button jbtguardar, jbtconsultar,jbtanular,jbtcancelar,jbtregresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+
+
+        getSupportActionBar().hide();
+
+        jetidentificacion=findViewById(R.id.etidentificacion);
+        jetnombre=findViewById(R.id.etnombre);
+        jetusuario=findViewById(R.id.etusuario);
+        jetclave1=findViewById(R.id.etclave1);
+        jetclave2=findViewById(R.id.etclave2);
+        jtvactico=findViewById(R.id.tvactivo);
+        jbtguardar=findViewById(R.id.btguardar);
+        jbtconsultar=findViewById(R.id.btconsultar);
+        jbtanular=findViewById(R.id.btanular);
+        jbtcancelar=findViewById(R.id.btcancelar);
+        jbtregresar=findViewById(R.id.btregresar);
+    }
+
+    public void Guardar(View view){
+
+        String identificacion,nombre,usuario,clave1,clave2;
+        identificacion=jetidentificacion.getText().toString();
+        nombre=jetnombre.getText().toString();
+        usuario=jetusuario.getText().toString();
+        clave1=jetclave1.getText().toString();
+        clave2=jetclave2.getText().toString();
+
+        if (identificacion.isEmpty() || nombre.isEmpty() || usuario.isEmpty() || clave1.isEmpty() || clave2.isEmpty()){
+            Toast.makeText(this, "Todos los datos son requeridos", Toast.LENGTH_SHORT).show();
+            jetidentificacion.requestFocus();
+
+        }
+        else{
+            if (clave1.equals(clave2)){
+                Toast.makeText(this, "Las claves no coninsiden", Toast.LENGTH_SHORT).show();
+                jetclave1.requestFocus();
+
+            }
+
+            else {
+                Conexion_concesionario admin=new Conexion_concesionario(this,"concesionario.bd",null,1);
+            }
+
+        }
+
     }
 }
